d5da7afcec8199e963f61b61e72730190dff62aa
KarsWeb <116569305+KarsWeb@users.noreply.github.com>
2024-01-03 15:37:47 +0900

    a
    a

--- js/script.js
+++ js/script.js
@@ -48,6 +48,12 @@ const swiper = new Swiper('#js-gallery-swiper', {
   // Optional parameters　　
   loop: true,
 
+  // 自動再生の設定
+  autoplay: {
+    delay: 3000, // 3秒ごとに切り替え
+    disableOnInteraction: true, // ユーザーのインタラクションで一時停止
+},
+
   // If we need pagination
   pagination: {
     el: '#js-gallery-pagination',
@@ -55,8 +61,8 @@ const swiper = new Swiper('#js-gallery-swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '#js-gallery-prev',
-    prevEl: '#js-gallery-next',
+    nextEl: '#js-gallery-next',
+    prevEl: '#js-gallery-prev',
   },
 });
 
@@ -128,9 +134,9 @@ jQuery('#js-drawer-content a[href^="#"]').on("click", function(e) {
 // トップへ戻るボタン100スクロールして表示
 jQuery(window).on("scroll", function() {
     if(100 < jQuery(window).scrollTop()) {
-        jQuery("#js-pagetop").addClass("is-show")
+        jQuery("#js-page-top").addClass("is-show")
     } else {
-        jQuery("#js-pagetop").removeClass("is-show")
+        jQuery("#js-page-top").removeClass("is-show")
     }
 })
 
@@ -149,3 +155,34 @@ const inViewItems = document.querySelectorAll(".js-in-view");
 inViewItems.forEach(function(inViewItem) {
     intersectionObserver.observe(inViewItem);
 });
+
+// ---------------------------------------
+// コンタクト　すべて入力時にアクティブにする
+
+document.addEventListener("DOMContentLoaded", function() {
+    const submitButton = document.getElementById('submit-button');
+    const inputs = document.querySelectorAll('.contact__form .form-text, .contact__form .form-select, .contact__form .form-textarea');
+
+    function updateButtonState() {
+        let allFilled = true;
+        inputs.forEach(input => {
+            if (input.value.trim() === '') {
+                allFilled = false;
+            }
+        });
+
+        submitButton.disabled = !allFilled;
+        if (allFilled) {
+            submitButton.classList.add('active');
+        } else {
+            submitButton.classList.remove('active');
+        }
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener('input', updateButtonState);
+    });
+
+    updateButtonState(); // 初期状態でボタンの状態を更新
+});
+
